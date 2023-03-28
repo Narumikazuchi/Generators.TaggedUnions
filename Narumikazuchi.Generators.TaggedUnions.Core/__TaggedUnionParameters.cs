34aeea181a5ca67925d760958770fcb0ac2e1a31
Narumikazuchi <ganzel.davy@swschwedt.de>
2023-03-28 17:58:49 +0200

    Implemented helper struct to generate errors when parameters intersect or invalid values have been entered.

---
+++ Narumikazuchi.Generators.TaggedUnions.Core/__TaggedUnionParameters.cs
@@ -0,0 +1,103 @@
+﻿using Narumikazuchi.TypeExtensions.Analyzers;
+
+namespace Narumikazuchi.TypeExtensions;
+
+internal record struct __TaggedUnionParameters(AttributeSyntax? Syntax, String Typename, HashSet<ITypeSymbol> Types)
+{
+    internal static __TaggedUnionParameters FromAttributeData(AttributeSyntax syntax,
+                                                              AttributeData data,
+                                                              out List<Diagnostic> diagnostics)
+    {
+        diagnostics = new();
+        HashSet<ITypeSymbol> types = new(SymbolEqualityComparer.Default);
+
+        if (data.ConstructorArguments[0].IsNull)
+        {
+            diagnostics.Add(TaggedUnionAnalyzer.CreateTypeIsNullDiagnostic(attribute: syntax,
+                                                                           index: 0));
+        }
+        else if (!types.Add((ITypeSymbol)data.ConstructorArguments[0].Value!))
+        {
+            diagnostics.Add(TaggedUnionAnalyzer.CreateDuplicateTypeDiagnostic(attribute: syntax,
+                                                                              index: 0));
+        }
+
+        if (data.ConstructorArguments[1].IsNull)
+        {
+            diagnostics.Add(TaggedUnionAnalyzer.CreateTypeIsNullDiagnostic(attribute: syntax,
+                                                                           index: 1));
+        }
+        else if (!types.Add((ITypeSymbol)data.ConstructorArguments[1].Value!))
+        {
+            diagnostics.Add(TaggedUnionAnalyzer.CreateDuplicateTypeDiagnostic(attribute: syntax,
+                                                                              index: 1));
+        }
+
+        if (data.ConstructorArguments.Length > 2 &&
+            !data.ConstructorArguments[2].IsNull)
+        {
+            Int32 index = 2;
+            foreach (TypedConstant type in data.ConstructorArguments[2].Values)
+            {
+                if (type.IsNull)
+                {
+                    diagnostics.Add(TaggedUnionAnalyzer.CreateTypeIsNullDiagnostic(attribute: syntax,
+                                                                                   index: index));
+                    continue;
+                }
+
+                if (!types.Add((ITypeSymbol)type.Value!))
+                {
+                    diagnostics.Add(TaggedUnionAnalyzer.CreateDuplicateTypeDiagnostic(attribute: syntax,
+                                                                                      index: index));
+                }
+
+                index++;
+            }
+        }
+
+        String name;
+        if (data.NamedArguments.Length > 0)
+        {
+            KeyValuePair<String, TypedConstant> typename = data.NamedArguments.Single(x => x.Key is "Typename");
+            if (typename.Value.IsNull)
+            {
+                diagnostics.Add(TaggedUnionAnalyzer.CreateTypenameIsNullDiagnostic(syntax));
+                return default;
+            }
+
+            name = (String)typename.Value.Value!;
+            if (String.IsNullOrWhiteSpace(name))
+            {
+                diagnostics.Add(TaggedUnionAnalyzer.CreateTypenameIsNullDiagnostic(syntax));
+                return default;
+            }
+        }
+        else
+        {
+            name = $"UnionOf_{String.Join("_", types.Select(x => x.Name))}";
+        }
+
+        return new(Syntax: syntax,
+                   Typename: name,
+                   Types: types);
+    }
+
+    internal Boolean ContentsEqual(__TaggedUnionParameters other)
+    {
+        if (this.Types.Count != other.Types.Count)
+        {
+            return false;
+        }
+
+        foreach (ITypeSymbol type in this.Types)
+        {
+            if (!other.Types.Contains(type))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
