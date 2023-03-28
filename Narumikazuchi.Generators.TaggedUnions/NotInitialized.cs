c9f348dc1f5f203ac540c3804b4d727c8fe6f0f0
Narumikazuchi <ganzel.davy@swschwedt.de>
2023-03-28 17:53:36 +0200

    Added excpetion for using uninitialized unions.

---
+++ Narumikazuchi.Generators.TaggedUnions/NotInitialized.cs
@@ -0,0 +1,21 @@
+﻿namespace Narumikazuchi.Generators.TaggedUnions;
+
+/// <summary>
+/// The exception that is thrown when trying to use the value of an uninitialized union.
+/// </summary>
+public sealed class NotInitialized : Exception
+{
+    /// <summary>
+    /// The exception that is thrown when trying to use the value of an uninitialized union.
+    /// </summary>
+    public NotInitialized()
+        : base("The type has not been initialized.")
+    { }
+
+    /// <summary>
+    /// The exception that is thrown when trying to use the value of an uninitialized union.
+    /// </summary>
+    public NotInitialized(String message)
+        : base(message)
+    { }
+}
