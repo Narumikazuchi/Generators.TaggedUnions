0336e616d5fc8b90adacf1b54da17f7d2094dd13
Narumikazuchi <ganzel.davy@swschwedt.de>
2023-03-28 17:53:24 +0200

    Added attribute to declare code generation parameters.

---
+++ Narumikazuchi.Generators.TaggedUnions/UnionOfAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace Narumikazuchi.Generators.TaggedUnions;
+
+/// <summary>
+/// Generates a tagged union of the specified types.
+/// </summary>
+[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true, Inherited = false)]
+public sealed class UnionOfAttribute : Attribute
+{
+    /// <summary>
+    /// Generates a tagged union of the specified types.
+    /// </summary>
+    public UnionOfAttribute(Type first,
+                            Type second,
+                            params Type[] additional)
+    { }
+
+    /// <summary>
+    /// The name to use for the generated type.
+    /// </summary>
+    public String? Typename
+    {
+        get;
+        set;
+    }
+}
