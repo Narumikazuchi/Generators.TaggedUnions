cde0c9c218591c8b8a6840ff431600062352dec4
Narumikazuchi <ganzel.davy@swschwedt.de>
2023-03-28 18:01:09 +0200

    Implemented analyzer.

---
+++ Narumikazuchi.Generators.TaggedUnions.Core/TaggedUnionAnalyzer.Diagnostics.cs
@@ -0,0 +1,74 @@
+﻿namespace Narumikazuchi.Generators.TaggedUnions.Analyzers;
+
+public partial class TaggedUnionAnalyzer
+{
+    static public Diagnostic CreateTypeIsNullDiagnostic(AttributeSyntax attribute,
+                                                        Int32 index)
+    {
+        return Diagnostic.Create(descriptor: s_TypeIsNullDescriptor,
+                                 location: attribute.ArgumentList!.Arguments[index].GetLocation());
+    }
+
+    static public Diagnostic CreateDuplicateTypeDiagnostic(AttributeSyntax attribute,
+                                                           Int32 index)
+    {
+        return Diagnostic.Create(descriptor: s_DuplicateTypeDescriptor,
+                                 location: attribute.ArgumentList!.Arguments[index].GetLocation());
+    }
+
+    static public Diagnostic CreateTypenameIsNullDiagnostic(AttributeSyntax attribute)
+    {
+        return Diagnostic.Create(descriptor: s_TypenameIsNullDescriptor,
+                                 location: attribute.ArgumentList!.Arguments.Last().GetLocation());
+    }
+
+    static public Diagnostic CreateTypenameExistsDiagnostic(AttributeSyntax attribute)
+    {
+        return Diagnostic.Create(descriptor: s_TypenameExistsDescriptor,
+                                 location: attribute.ArgumentList!.Arguments.Last().GetLocation());
+    }
+
+    static public Diagnostic CreateUnionExistsDiagnostic(AttributeSyntax attribute,
+                                                        String typename)
+    {
+        return Diagnostic.Create(descriptor: s_UnionExistsDescriptor,
+                                 location: attribute.GetLocation(),
+                                 typename);
+    }
+
+    static private readonly DiagnosticDescriptor s_TypeIsNullDescriptor = new(id: "NCG001",
+                                                                              category: "Code Generation",
+                                                                              title: "Union of 'null'",
+                                                                              messageFormat: "A tagged union of type 'null' is not allowed.",
+                                                                              description: "A tagged union of type 'null' is not allowed.",
+                                                                              defaultSeverity: DiagnosticSeverity.Error,
+                                                                              isEnabledByDefault: true);
+    static private readonly DiagnosticDescriptor s_DuplicateTypeDescriptor = new(id: "NCG002",
+                                                                                 category: "Code Generation",
+                                                                                 title: "Union of identical types",
+                                                                                 messageFormat: "A tagged union of identical types is not allowed.",
+                                                                                 description: "A tagged union of type identical types will result in the exact same type.",
+                                                                                 defaultSeverity: DiagnosticSeverity.Error,
+                                                                                 isEnabledByDefault: true);
+    static private readonly DiagnosticDescriptor s_TypenameIsNullDescriptor = new(id: "NCG003",
+                                                                                 category: "Code Generation",
+                                                                                 title: "Typename is null or empty",
+                                                                                 messageFormat: "A tagged union can not be created with an empty typename or a typename of 'null'.",
+                                                                                 description: "A tagged union can not be created with an empty typename or a typename of 'null'.",
+                                                                                 defaultSeverity: DiagnosticSeverity.Error,
+                                                                                 isEnabledByDefault: true);
+    static private readonly DiagnosticDescriptor s_TypenameExistsDescriptor = new(id: "NCG004",
+                                                                                  category: "Code Generation",
+                                                                                  title: "Typename already exists",
+                                                                                  messageFormat: "A tagged union can not be created with this typename since another type with that name already exists.",
+                                                                                  description: "A tagged union can not be created with this typename since another type with that name already exists.",
+                                                                                  defaultSeverity: DiagnosticSeverity.Error,
+                                                                                  isEnabledByDefault: true);
+    static private readonly DiagnosticDescriptor s_UnionExistsDescriptor = new(id: "NCG005",
+                                                                               category: "Code Generation",
+                                                                               title: "Union already exists",
+                                                                               messageFormat: "A tagged union consisting of the specified types already exists as '{0}'.",
+                                                                               description: "A tagged union consisting of the specified types already exists.",
+                                                                               defaultSeverity: DiagnosticSeverity.Error,
+                                                                               isEnabledByDefault: true);
+}
---
+++ Narumikazuchi.Generators.TaggedUnions.Core/TaggedUnionAnalyzer.cs
@@ -0,0 +1,128 @@
+﻿namespace Narumikazuchi.Generators.TaggedUnions.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed partial class TaggedUnionAnalyzer : DiagnosticAnalyzer
+{
+    public override void Initialize(AnalysisContext context)
+    {
+        m_Marked.Clear();
+        m_TaggedUnions.Clear();
+
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+        context.RegisterSyntaxNodeAction(action: this.Analyze,
+                                         SyntaxKind.Attribute/*,
+                                         SyntaxKind.AttributeList,
+                                         SyntaxKind.AttributeArgument,
+                                         SyntaxKind.AttributeArgumentList*/);
+    }
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = new DiagnosticDescriptor[]
+    {
+        s_TypeIsNullDescriptor,
+        s_DuplicateTypeDescriptor,
+        s_TypenameIsNullDescriptor,
+        s_TypenameExistsDescriptor,
+        s_UnionExistsDescriptor
+    }.ToImmutableArray();
+
+    private void Analyze(SyntaxNodeAnalysisContext context)
+    {
+        if (context.Node is AttributeSyntax attribute)
+        {
+            this.AnalyzeAttribute(context: context,
+                                  attribute: attribute);
+        }
+    }
+
+    private void AnalyzeAttribute(SyntaxNodeAnalysisContext context,
+                                  AttributeSyntax attribute)
+    {
+        IMethodSymbol constructorSymbol = (IMethodSymbol)context.SemanticModel.GetSymbolInfo(attribute).Symbol!;
+        INamedTypeSymbol attributeSymbol = constructorSymbol.ContainingType;
+        String fullName = attributeSymbol.ToDisplayString();
+        if (fullName is not Generators.TaggedUnionGenerator.UNION_ATTRIBUTE_FULLNAME)
+        {
+            return;
+        }
+
+        SemanticModel semanticModel = context.SemanticModel;
+        SyntaxReference reference = attribute.GetReference();
+        AttributeData data = context.Compilation.Assembly.GetAttributes()
+                                                         .Single(x => reference.SyntaxTree == x.ApplicationSyntaxReference?.SyntaxTree &&
+                                                                      reference.Span == x.ApplicationSyntaxReference?.Span);
+
+        ImmutableArray<AttributeData> otherDeclarations = context.Compilation.Assembly.GetAttributes()
+                                                                                      .Where(data => data.AttributeClass is not null &&
+                                                                                                     data.AttributeClass.ToDisplayString() is Generators.TaggedUnionGenerator.UNION_ATTRIBUTE_FULLNAME &&
+                                                                                                     (reference.SyntaxTree != data.ApplicationSyntaxReference?.SyntaxTree ||
+                                                                                                     reference.Span != data.ApplicationSyntaxReference?.Span))
+                                                                                      .ToImmutableArray();
+
+        __TaggedUnionParameters unionParameters = __TaggedUnionParameters.FromAttributeData(syntax: attribute,
+                                                                                            data: data,
+                                                                                            diagnostics: out List<Diagnostic> diagnostics);
+
+        if (diagnostics.Count > 0)
+        {
+            foreach (Diagnostic diagnostic in diagnostics)
+            {
+                context.ReportDiagnostic(diagnostic);
+            }
+
+            return;
+        }
+
+        ImmutableArray<__TaggedUnionParameters> otherUnions = otherDeclarations.Select(GetUnionParameters)
+                                                                               .ToImmutableArray();
+
+        ImmutableArray<__TaggedUnionParameters> nameDuplicates = otherUnions.Where(other => other.Typename == unionParameters.Typename)
+                                                                            .ToImmutableArray();
+        if (nameDuplicates.Length > 0)
+        {
+            context.ReportDiagnostic(CreateTypenameExistsDiagnostic(attribute));
+        }
+
+        ImmutableArray<__TaggedUnionParameters> typeDuplicates = otherUnions.Where(other => TypesListsAreEqual(other, unionParameters))
+                                                                            .ToImmutableArray();
+        if (typeDuplicates.Length > 0)
+        {
+            foreach (__TaggedUnionParameters typeDuplicate in typeDuplicates)
+            {
+                context.ReportDiagnostic(CreateUnionExistsDiagnostic(attribute: attribute,
+                                                                     typename: typeDuplicate.Typename));
+            }
+        }
+    }
+
+    static private __TaggedUnionParameters GetUnionParameters(AttributeData data)
+    {
+        if (data.ApplicationSyntaxReference is null)
+        {
+            return default;
+        }
+
+        AttributeSyntax syntax = (AttributeSyntax)data.ApplicationSyntaxReference.GetSyntax();
+        return __TaggedUnionParameters.FromAttributeData(syntax: syntax,
+                                                         data: data,
+                                                         diagnostics: out _);
+    }
+
+    static private Boolean TypesListsAreEqual(__TaggedUnionParameters left,
+                                              __TaggedUnionParameters right)
+    {
+        if (left.Types.Count > right.Types.Count)
+        {
+            return !left.Types.Except(right.Types)
+                              .Any();
+        }
+        else
+        {
+            return !right.Types.Except(left.Types)
+                               .Any();
+        }
+    }
+
+    private readonly List<String> m_Marked = new();
+    private readonly List<__TaggedUnionParameters> m_TaggedUnions = new();
+}
